d492107a8f6cc29f1ec85431db6817283d28dad0
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-03-16 17:38:08 -0300

    Adiciona o 5° requisito do Projeto Zoo Functions

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
+const defaultReturn = {};
+species.forEach((animal) => {
+  defaultReturn[animal.name] = animal.residents.length;
+});
+
+function countAnimals(animal = defaultReturn) {
   // seu código aqui
+  if (animal === defaultReturn) return defaultReturn;
+  if (animal.sex !== undefined) {
+    return species.find((specie) => specie.name === animal.specie)
+      .residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return species.find((specie) => specie.name === animal.specie).residents.length;
 }
 
 module.exports = countAnimals;
