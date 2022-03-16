9584cf10fe6c7fc533922860c505f7f43a9cf74b
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-03-16 15:04:54 -0300

    Adiciona o 1° requisito do Projeto Zoo Functions

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return ids.map((id) => data.species.find((animal) => animal.id === id));
 }
 
 module.exports = getSpeciesByIds;
