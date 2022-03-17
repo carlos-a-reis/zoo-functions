14ad26ea73ee5c9830f5cf9805e179702d1c2857
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-03-17 14:12:05 -0300

    Adiciona o 9° requisito do Projeto Zoo Functions

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const animalId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animals = species.find((specie) => specie.id === animalId).residents;
+  const maxAge = Math.max(...animals.map((animal) => animal.age));
+  const olderAnimal = animals.find((animal) => animal.age === maxAge);
+  return [olderAnimal.name, olderAnimal.sex, olderAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
